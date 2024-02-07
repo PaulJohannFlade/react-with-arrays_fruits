be8df84b52051a245059cf01836e3104ff87c3d8
Paul Johann Flade <paul-johann@flade-privat.de>
2024-02-07 11:05:02 +0100

    chalange solved

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      id: "1",
+      name: "🍌 Banana",
+      color: "yellow",
+    },
+    {
+      id: "2",
+      name: "🍎 Apple",
+      color: "red",
+    },
+    {
+      id: "3",
+      name: "🍑 Peach",
+      color: "orange",
+    },
+    {
+      id: "4",
+      name: "🥭 Mango",
+      color: "moccasin",
+    },
+    {
+      id: "5",
+      name: "🥝 Kiwi",
+      color: "green",
+    },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map((fruit) => (
+        <Card
+          key={fruit.id}
+          id={fruit.id}
+          name={fruit.name}
+          color={fruit.color}
+        />
+      ))}
     </div>
   );
 }
